45bda82cb7211028e2606ce4bf33ea21669b0531
Ommooley10 <omvikas1001@gmail.com>
2025-06-03 00:06:38 +0530

    frontend setup completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
@@ -9,6 +10,10 @@ import { connectDB } from './lib/db.js';
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true //alow frontend to send cookies
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);

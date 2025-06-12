abd7baab5f027731c8f8f363a179294b12017296
Ommooley10 <omvikas1001@gmail.com>
2025-06-13 00:13:37 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+//Use localhost:5001 while development and /api when in production
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 
 export const axiosInstance = axios.create({
